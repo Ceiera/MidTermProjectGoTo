3ee6e397bf93a28a1f58c78541ef86e25f339989
JuruLOSS <32490299+Ceiera@users.noreply.github.com>
2023-08-14 11:16:57 +0700

    add cors

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
-require('dotenv').config()
-const express = require("express")
-const mongoose = require('mongoose')
-const mongoString = process.env.DATABASE_URL
-const videoRoutes = require('./src/routes/videos.route')
-const productRoutes = require('./src/routes/products.route')
-const commentRoutes = require('./src/routes/comments.route')
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const mongoString = process.env.DATABASE_URL;
+const videoRoutes = require("./src/routes/videos.route");
+const productRoutes = require("./src/routes/products.route");
+const commentRoutes = require("./src/routes/comments.route");
+const cors = require("cors");
 
-mongoose.connect(mongoString)
+mongoose.connect(mongoString);
 const db = mongoose.connection;
 
-db.on('error', (err) => {
-    console.log(err)
+db.on("error", (err) => {
+  console.log(err);
 });
 
-db.once('connected', () => {
-    console.log(`DB Connected`)
+db.once("connected", () => {
+  console.log(`DB Connected`);
 });
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use('/videos', videoRoutes)
-app.use('/products', productRoutes)
-app.use('/comments', commentRoutes)
+app.use("/videos", videoRoutes);
+app.use("/products", productRoutes);
+app.use("/comments", commentRoutes);
 
 const listener = app.listen(80, function () {
-  console.log("Listening on port " + listener.address().port)
+  console.log("Listening on port " + listener.address().port);
 });
